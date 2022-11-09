10ca2d32cc03e706a5490ea1ddb198bb9b03a6bd
iskender.samatuulu <iskender.samatuulu@nascdev.com>
2022-11-09 20:51:01 +0600

    started learn DOM

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let posTitle  = document.getElementsByClassName('posTitle')[0];
+
+console.log(typeof posTitle)
+
+function displayDate(){
+    let element = document.getElementsByClassName('posTitle')[0];
+    element.innerHTML = Date();
+    element.style.color = 'black';
+    element.style.fontSize = '20px';
+}
+function upperCase(){
+    let element = document.getElementsByName('firstName')[0];
+    element.value = element.value.toUpperCase()
+}
