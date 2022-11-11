ac5307614ba2afec969e606aa9c9114c57d67e7b
Begimai <begimai.modueva.ch22@gmail.com>
2022-11-11 10:17:13 +0300

    DOM

---
+++ js/jsscript.js
@@ -0,0 +1,45 @@
+// let posTitle = document.getElementsByClassName('posTitle')[0];
+
+// // let header = document.getElementsByName('header')[0];
+
+// let header = document.getElementsByClassName('header')[0];
+
+// // let positions = document.getElementById('positions');
+
+// let positions = document.querySelector('#positions');
+
+// header.style.backgroundColor = 'red';
+
+// positions.style.color = '#000';
+
+// posTitle.style.color = 'red';
+
+
+// console.log(posTitle);
+// console.log(header);
+// console.log(positions);
+
+
+// function displayDate() {
+//     let element = document.getElementsByClassName('posTitle')[0];
+//     element.innerHTML = Date();
+//     element.style.color = 'black';
+//     element.style.fontSize = "15px"
+// }
+
+// function upperCase() {
+//     let element = document.getElementsByTagName('firstName')[0];
+//     element.value = element.value.toUpperCase()
+// }
+
+
+//дз
+
+let copyright = document.getElementsByClassName('copyright')[0];
+copyright.style.color = 'red';
+
+console.log(copyright);
+
+function changeText(id) {
+    id.innerHTML = "НЕ ДЕЛАЙ ТАК !";
+}
