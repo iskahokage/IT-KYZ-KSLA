ea9e87d4a65a365806373b5f349b32067cb0dd6a
iskender.samatuulu <iskender.samatuulu@nascdev.com>
2022-11-30 15:47:13 +0600

    events and DOM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let posTitle  = document.getElementsByClassName('posTitle')[0];
 
-console.log(typeof posTitle)
+// console.log(typeof posTitle)/
 
 function displayDate(){
     let element = document.getElementsByClassName('posTitle')[0];
@@ -11,4 +11,50 @@ function displayDate(){
 function upperCase(){
     let element = document.getElementsByName('firstName')[0];
     element.value = element.value.toUpperCase()
+}
+
+
+let element_container = document.querySelector('.element_container');
+
+element_container.style.display = 'flex';
+
+element_container.style.justifyContent = 'space-around';
+
+element_container.style.padding = '20px';
+
+
+let btn_on = document.createElement('button');
+let btn_off = document.createElement('button');
+
+btn_on.innerText = 'Включить темную тему'
+btn_off.innerText = 'Отключить темную тему'
+
+element_container.append(btn_on)
+element_container.append(btn_off)
+
+btn_on.addEventListener('click', ()=>{
+    mask.classList.add('dark-theme')
+})
+
+btn_off.addEventListener('click', (e)=>{
+    mask.classList.remove('dark-theme')
+    // console.log(e)
+})
+
+document.addEventListener('keydown', (e)=>{
+    if(e.code === 'KeyA'){
+        mask.classList.add('dark-theme')
+    }else if(e.code === 'KeyB'){
+        mask.classList.remove('dark-theme')
+    }
+})
+
+
+
+let mask = document.querySelector('.mask');
+
+let time = new Date().getHours();
+
+if(time > 17){
+    mask.classList.add('dark-theme')
 }
